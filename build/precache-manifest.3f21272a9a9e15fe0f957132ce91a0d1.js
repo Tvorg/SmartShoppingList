a78d0828bd20a535adf9a5e27dbb694a5478a256
Aleksandr Ihnatenko <60549237+Tvorg@users.noreply.github.com>
2020-10-01 01:08:16 +0300

    Add files via upload

---
+++ build/precache-manifest.3f21272a9a9e15fe0f957132ce91a0d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9780dc9c44199991beece4392ba3e2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab1bc31140346761b69",
+    "url": "/static/css/main.10059e8a.chunk.css"
+  },
+  {
+    "revision": "1dd2d44466c6c405da8d",
+    "url": "/static/js/2.39162f41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39162f41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab1bc31140346761b69",
+    "url": "/static/js/main.9bd3b827.chunk.js"
+  },
+  {
+    "revision": "2b6fa35141b5c2163019",
+    "url": "/static/js/runtime-main.6c42eb61.js"
+  }
+]);
